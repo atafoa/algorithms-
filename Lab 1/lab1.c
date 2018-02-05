45dc6730aae4bf9a0a3956449b0add57e696dd69
atafoabure <atafoa@yahoo.com>
2018-02-05 12:41:15 -0600

    Update feb5

--- Lab 1/lab1.c
+++ Lab 1/lab1.c
@@ -2,8 +2,10 @@
 #include <limits.h>
 #include <stdlib.h>
 
-int rankNumbers(int l[],int ll[],int m,int n,int p);
-int cmpfunc (const void * a, const void * b);
+
+void ranksByMerge(int m, int n, int probe, int *a, int *b);
+int binarySearch(int *a,int n,int key);
+
 
 
 int main(){
@@ -13,17 +15,17 @@ int main(){
 	scanf("%d",&n);
 	scanf("%d",&p);
 	
-	int i, j, *a, *b, rank;
+	int i, j, *a, *b, probe;
 	a = (int *)malloc((m+2) * sizeof(int));
 	b = (int *)malloc((n+2) * sizeof(int));
-
+	
 	for(i = 1; i < m+1; i++)
 	{
 		scanf("%d",&a[i]);
 	}
 	a[0] = -99999999;
 	a[m+1] = 99999999;
-
+	
 	for(j = 1; j < n+1; j++)
 	{
 		scanf("%d",&b[j]);
@@ -36,50 +38,63 @@ int main(){
 		printf("%d",a[i]);
 	}
 	
-//	int probe;
-//	qsort(a,m,sizeof(int),cmpfunc);
-//	qsort(b,m,sizeof(int),cmpfunc);
-//
-//
-//	scanf("%d", &probe);
-//	rank = rankNumbers(a, b, m, n, probe);
-//	printf("Rank is : %d \n",rank);
+	scanf("%d",&probe);
 	
 	return 0;
 }
+void ranksByMerge(int m, int n, int probe, int *a, int *b) // finds rank of each number at probe
+{
+	int i,j,k;
+	i=j=k=1;
+	
+	while (i<=m && j<=n)
+	{	if (a[i]<=b[j])
+		{ if( probe == k)
+			{
+				binarySearch(a, m, probe);
+			}
+			k++;
+		}
+		else
+		{
+			if(probe == k)
+			{
+				binarySearch(b, n, probe);
+			}
+			k++;
+		}
+		
+		i++;
+		j++;
+	}
+	while (i<=m)
+		k++;
+	while (j<=n)
+		k++;
+}
+int binarySearch(int *a,int n,int key)
+// Input: int array a[] with n elements in ascending order.
+//        int key to find.
+// Output: Returns some subscript of a where key is found.
+//         Returns -1 if not found.
+// Processing: Binary search.
+{
+	int low,high,mid;
+	low=0;
+	high=n-1;
+	// subscripts between low and high are in search range.
+	// size of range halves in each iteration.
+	while (low<=high)
+	{
+		mid=(low+high)/2;
+		if (a[mid]==key)
+			return mid; // key found
+		if (a[mid]<key)
+			low=mid+1;
+		else
+			high=mid-1;
+	}
+	
+	return (-1); // key does not appear
+}
 
-
-//int cmpfunc (const void * a, const void * b)
-//{
-//	return ( *(int*)a - *(int*)b );
-//}
-//
-//int rankNumbers(int l[],int ll[],int m,int n,int p)
-//{
-//	while (1){
-//		int i = (int)((float)m/(m+n)*(p-1));
-//		int j = p-1-i;
-//
-//		int Ai_1 = (i == 0) ? INT_MIN : l[i-1];
-//		int Ai = (i == m) ? INT_MAX : l[i];
-//		int Bj_1 = (j == 0) ? INT_MIN : ll[j-1];
-//		int Bj = (j == n) ? INT_MAX : ll[j];
-//
-//		if (Bj_1 < Ai && Bj > Ai) return Ai;
-//		if (Ai_1 < Bj && Ai > Bj) return Bj;
-//
-//		if (Ai < Bj){
-//			l = l + i + 1;
-//			m = m - i - 1;
-//			p = p - i - 1;
-//		}
-//		else{
-//			ll = ll + j + 1;
-//			n = n - j - 1;
-//			p = p - j - 1;
-//		}
-//	}
-//}
-//
-//
-//
