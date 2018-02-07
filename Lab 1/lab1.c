a4a62d1bfb6ca7d7fb9abbfd31d245eaa9303401
atafoabure <atafoa@yahoo.com>
2018-02-06 20:17:00 -0600

    Turned In version

--- Lab 1/lab1.c
+++ Lab 1/lab1.c
@@ -1,5 +1,6 @@
 /* Lab 1
  
+ CSE 2320 - Lab 1
  Created By Atafo Abure
  Feb 6 2018
 
@@ -21,6 +22,7 @@ int main(){
 	int high, low;
 	int x,y;
 	
+	//dynamic allocation for arrays
 	a = (int *)malloc((m+2) * sizeof(int));
 	b = (int *)malloc((n+2) * sizeof(int));
 	
@@ -38,16 +40,17 @@ int main(){
 	b[0] = -99999999;
 	b[n+1] = 99999999;
 	
-	if(p > m || p > n)
+	if(p > m || p > n) // Test case to account for the number of probes being larger than the size of both arrays
 	{
-		printf("Number of probes given are larger than the sizes of every array"); 
-		return 0;
+		printf("Number of probes given are larger than the sizes of every array");
+		return 0; // quits the program
 	}
 	
-	for(k = 0; k < p; k++)
+	for(k = 0; k < p; k++) // run the program for the given number of ranks
 	{
-		scanf("%d",&rank);
+		scanf("%d",&rank); // get the ranks from the input files
 		
+		//if statement to find which array is the larger array
 		if(m > n)
 		{	smallestSize = n;
 			biggestSize = m;
@@ -57,16 +60,17 @@ int main(){
 			biggestSize = n;
 		}
 		
+		// if rank is greater than the size of the smallest array then high value should be set to the smallest array and m has to be greater than or equal to n
 		if(rank > smallestSize && m <= n)
 			high = smallestSize;
 		else
 			high = rank;
 	
-		low = rank - biggestSize;
+		low = rank - biggestSize; // low should be the rank minus the biggestSize if the value is negative your lowest value should be 0
 		if(low < 0)
 			low = 0;
 		
-		binarySearch(a, b, low, high, rank);
+		binarySearch(a, b, low, high, rank); // call binary search function
 	   }
 	return 0;
 }
@@ -86,7 +90,7 @@ void binarySearch(int *a,int *b, int low, int high, int rank)
 		printf("i is: %d j is: %d ",i,j);
 		printf("a[%d] is: %d b[%d] is: %d\n",i,a[i],j,b[j]);
 		
-		mid=(low+high)/2;
+		mid=(low+high)/2; //mid splits the array in half for the purpose of binary search
 		
 		if(i + j ==  rank)
 		{
@@ -94,24 +98,24 @@ void binarySearch(int *a,int *b, int low, int high, int rank)
 			{
 				if(a[i] <= b[j+1])
 				{
-					printf("a[%d] = %d has rank %d\n",i,a[i],rank);
+					printf("a[%d] = %d has rank %d\n",i,a[i],rank); // the given rank is in arrayA at position i
 					break;
 				}
 				else
 				{
-					high = mid - 1;
+					high = mid - 1; // reset high value to mid - 1
 				}
 			}
 			else if(a[i] <= b[j])
 			{
 				if(b[j] < a[i+1])
 				{
-					printf("b[%d] = %d has rank %d\n",j,b[j],rank);
+					printf("b[%d] = %d has rank %d\n",j,b[j],rank); // the given rank is in arrayB at position j
 					break;
 				}
 				else
 				{
-					low = mid+1;
+					low = mid+1; // reset low value to mid + 1
 				}
 			}
 		}
