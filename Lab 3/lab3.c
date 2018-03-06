30059999d543f5cdf029b5ce2c0be85a46237400
atafoabure <atafoa@yahoo.com>
2018-03-06 12:30:08 -0600

    C file created

---
+++ Lab 3/lab3.c
@@ -0,0 +1,21 @@
+/* Lab 3
+ 
+ CSE 2320 - Lab 3
+ Created by Atafo Abure 1001442575
+ 
+ This program implements order-preserving Huffman coding using the dynamic programming formulation
+ 
+ How to run
+ To compile and run use the following commands
+ gcc lab3.c
+ a.out < inputFile.txt
+ 
+ inputFile.txt would be the name of the input file you want to work with.
+ Note the inputFile has to be in the same directory as the lab3.c file
+ */
+
+#include <stdio.h>
+
+int main() {
+	return 0;
+}
