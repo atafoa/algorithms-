20f33ba026c0a342f38db0c458a0578fbf02f9f7
atafoabure <atafoa@yahoo.com>
2018-03-07 01:09:00 -0600

    Tuesday

--- Lab 3/lab3.c
+++ Lab 3/lab3.c
@@ -14,8 +14,70 @@
  Note the inputFile has to be in the same directory as the lab3.c file
  */
 
+// Optimal matrix multiplication order using dynamic programming
 #include <stdio.h>
+#include <stdlib.h>
+//void tree(int left,int right,int indent);
 
-int main() {
-	return 0;
+
+
+struct Test
+{
+	double probability;
+	int symbol;
+};
+
+int main()
+{
+	struct Test *tests;
+	int n;
+	scanf("%d",&n);
+	//double p[n];
+	double c[n][n];
+	int trace[n][n];
+	
+	tests = (struct Test *)malloc(n*sizeof(struct Test));
+	
+	int i,j,k;
+	double work;
+	
+	for (i=0;i<=n;i++)
+	{	scanf("%lf",&tests[i].probability);
+		tests[i].symbol = i;
+	}
+	
+	for (i=1;i<=n;i++)
+		c[i][i]=trace[i][i]=0;
+	
+	for (i=1;i<n;i++)
+		for (j=1;j<=n-i;j++)
+		{
+			c[j][j+i]=999999;
+			for (k=j;k<j+i;k++)
+			{
+				work=tests[j].probability+c[j][k]+c[k+1][j+i];
+				if (c[j][j+i]>work)
+				{
+					c[j][j+i]=work;
+					trace[j][j+i]=k;
+				}
+			}
+		}
+	
+	printf("   ");
+	for (i=1;i<=n;i++)
+		printf("   %3d   ",i);
+	printf("\n");
+	for (i=1;i<=n;i++)
+	{
+		printf("%2d ",i);
+		for (j=1;j<=n;j++)
+			if (i>j)
+				printf(" ------- ");
+			else
+				printf(" %.1lf %3d ",c[i][j],trace[i][j]);
+		printf("\n");
+		
+		printf("\n");
+	}
 }
