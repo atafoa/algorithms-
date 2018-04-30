9a2494df4fcb748ac533d16c06c1b1efdc061865
atafoabure <atafoa@yahoo.com>
2018-04-30 17:51:58 -0500

    Makefile created

--- Lab 6/lab6.c
+++ Lab 6/lab6.c
@@ -2,11 +2,30 @@
 	CSE 2320 Lab 6
 	Atafo Abure
 
-	This program reads a string for a serialized red black tree, deserializes it and perfoorms some insersertions, then serializes the revised tree
+	This program uses the “pebble game” technique to test if an input graph is minimally rigid (in two dimensions).
 */
 
+int** sentinel(int size);
 
 int main()
 {
+    int n;
+    scanf("%d",&n);
+    int undirectedEdges = sentinel(n);
+
     return 0;
+}
+
+int** sentinel(int size)
+{
+    int edgeSize = 2*size - 3;
+    int undirectedEdges[size][2];
+    int i = 0;
+    for(i = 0; i < size; i++)
+    {
+        scanf("%d %d",undirectedEdges[i][0], undirectedEdges[i][1]);
+    }
+
+    return undirectedEdges;
+
 }
