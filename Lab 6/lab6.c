45005f782915a6e112a989aa101f2b8191062fe2
atafoabure <atafoa@yahoo.com>
2018-04-30 17:58:40 -0500

    undirected edges created

--- Lab 6/lab6.c
+++ Lab 6/lab6.c
@@ -5,27 +5,21 @@
 	This program uses the “pebble game” technique to test if an input graph is minimally rigid (in two dimensions).
 */
 
-int** sentinel(int size);
+#include <stdio.h>
+
 
 int main()
 {
     int n;
     scanf("%d",&n);
-    int undirectedEdges = sentinel(n);
-
-    return 0;
-}
-
-int** sentinel(int size)
-{
-    int edgeSize = 2*size - 3;
-    int undirectedEdges[size][2];
+    int edgeSize = 2*n - 3;
+    int undirectedEdges[n][2];
     int i = 0;
-    for(i = 0; i < size; i++)
+    for(i = 0; i < edgeSize; i++)
     {
-        scanf("%d %d",undirectedEdges[i][0], undirectedEdges[i][1]);
+        scanf("%d %d",&undirectedEdges[i][0], &undirectedEdges[i][1]);
+        printf("%d %d\n",undirectedEdges[i][0], undirectedEdges[i][1]);
     }
 
-    return undirectedEdges;
-
+    return 0;
 }
